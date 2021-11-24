18f542dde2cda3b70c948208a2eabf473e914f6e
James Driscoll <thejamesdriscoll@gmail.com>
2021-11-24 10:11:51 -0600

    form can only be submitted if passwords match

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ const password = document.querySelector('[data-password]')
 const confirmPassword = document.querySelector('[data-password-confirm]')
 const email = document.querySelector('[data-email]')
 const message = document.querySelector('[data-validation-message]')
-function validatePassword(pass,confirm) {
+const form = document.querySelector('[data-form]')
+let validPass = false
+function validatePassword(pass, confirm) {
     confirm.addEventListener('blur', () => {
         if (pass.value !== "" && pass.value === confirm.value) {
-            
             message.innerHTML = "Passwords match!"
+            validPass = true
+            onSubmit()
         } else if (pass.value !== "" && pass.value !== confirm.value) {
             message.innerHTML = "Passwords do not match."
-        }
+            validPass = false
+            onSubmit()
+        } else return validPass
     })
-
 }
+
 function checkValidity(form) {
     form.addEventListener('blur', () => {
         if (!form.checkValidity()) {
@@ -21,7 +26,16 @@ function checkValidity(form) {
             message.innerHTML = "Input OK"
         }
     })
-
 }
+
+function onSubmit(e) {
+    if (validPass) {
+        return
+    } else if (!validPass) {
+        e.preventDefault()
+    }
+}
+
+form.addEventListener('submit', onSubmit)
 checkValidity(email)
-validatePassword(password,confirmPassword)
+validatePassword(password, confirmPassword)
